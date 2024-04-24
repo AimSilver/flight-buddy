dffea34b2ef5ec883df5604a5332f781bec107e2
AimSilver <135525162+AimSilver@users.noreply.github.com>
2024-04-24 10:24:22 +0530

    flight buddy

---
+++ backend/Models/FlightManager.js
@@ -0,0 +1,31 @@
+const Flight = require("./flightModel");
+const FlightDetail = require("/Users/ashis/Desktop/project 2/backend/Models/flightDetail");
+
+class FlightManager {
+  calculatePrice(
+    source,
+    destination,
+    numberOfAdults,
+    numberOfChildren,
+    dateOfTravel
+  ) {
+    const flightDetail = new FlightDetail();
+    let basePrice = flightDetail.getBasePrice(
+      source,
+      destination,
+      dateOfTravel
+    );
+    if (typeof basePrice == "number") {
+      let totalPrice = 0;
+
+      const totalPassengers = numberOfAdults + numberOfChildren;
+      totalPrice = totalPassengers * basePrice;
+
+      let price = basePrice; // Set the calculated price
+      return totalPrice;
+    } else if (typeof basePrice == "string") {
+      return basePrice;
+    }
+  }
+}
+module.exports = FlightManager;
---
+++ backend/Models/flightDetail.js
@@ -0,0 +1,24 @@
+class FlightDetails {
+  constructor() {
+    this.routePrices = [
+      {
+        route: "New York-London",
+        price: 200,
+      },
+      { route: "London-Paris", price: 180 },
+      { route: "default", price: 150 }, // Default price
+      // Add more routes as needed
+    ];
+  }
+
+  getBasePrice(source, destination, date) {
+    const route = `${source}-${destination}`;
+    const foundRoute = this.routePrices.find((r) => r.route === route);
+
+    return foundRoute
+      ? foundRoute.price
+      : this.routePrices.find((r) => r.route === "default").price;
+  }
+}
+
+module.exports = FlightDetails;
